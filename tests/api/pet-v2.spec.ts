1c9f09379c8569f1b54cdf3901f9701ee3634f1d
J K <me@jameskip.com>
2023-03-29 13:53:57 -0500

    Add CRUD operations for pet endpoint

---
+++ tests/api/pet-v2.spec.ts
@@ -0,0 +1,61 @@
+import { test, expect } from "@playwright/test";
+import { getRandomNumber } from "../../utils/helpers";
+
+test.use({
+  baseURL: "https://petstore.swagger.io",
+});
+
+const petId = getRandomNumber(1, 1000);
+
+const expectedResponse = {
+  category: { id: 0, name: "string" },
+  id: petId,
+  name: `doggie ${petId}`,
+  photoUrls: ["string"],
+  status: "available",
+  tags: [{ id: 0, name: "string" }],
+};
+
+// CREATE pet before all tests are run
+test.beforeAll(async ({ request }) => {
+  await request.post(`/v2/pet`, {
+    headers: {
+      "Content-Type": "application/json",
+      accept: "application/json",
+    },
+    data: JSON.stringify(expectedResponse),
+  });
+});
+
+// DELETE pet after all tests are done
+test.afterAll(async ({ request }) => {
+  await request.delete(`/v2/pet/${petId}`, {
+    headers: { api_key: "special-key" },
+  });
+});
+
+test(`CRUD - Pet`, ({ request }) => {
+  // READ pet by id
+  test.step(`GET - doggie`, async () => {
+    const response = await request.get(`/v2/pet/${petId}`);
+
+    expect(await response.json()).toEqual(
+      expect.objectContaining(expectedResponse)
+    );
+  });
+
+  // UPDATE pet status
+  test.step(`PUT - doggie`, async () => {
+    const response = await request.put(`/v2/pet`, {
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+      data: JSON.stringify({ ...expectedResponse, status: "sold" }),
+    });
+
+    expect(await response.json()).toEqual(
+      expect.objectContaining({ ...expectedResponse, status: "sold" })
+    );
+  });
+});
