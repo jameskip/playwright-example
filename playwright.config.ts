0b03b09ddeea5f14c823abe63f228898b66b6031
J K <me@jameskip.com>
2023-03-30 00:08:42 -0500

    revert

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ module.exports = defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? "junit" : "html",
+  reporter: "html",
   use: {
     baseURL: "https://www.saucedemo.com/",
     trace: "on",
