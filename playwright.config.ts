74e5ae432a09515da354e98f69e28472ec08aa7d
J K <me@jameskip.com>
2023-03-30 00:29:46 -0500

    Update reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ module.exports = defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
+  reporter: [["html"], ["junit", { outputFile: "test-results.xml" }]],
   use: {
     baseURL: "https://www.saucedemo.com/",
     trace: "on",
